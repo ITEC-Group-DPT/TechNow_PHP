93bf599d85a60c746b44ec303ca72cbcb8b9d4b8
chanhbeck <chanhbeck27@gmail.com>
2021-06-30 19:12:22 +0700

    some func :sadaf:

--- cart.php
+++ cart.php
@@ -3,8 +3,6 @@
   include 'classes/Cart.php';
   $cart = new Cart($conn);
 
-
-
   include 'includes/header.php';
 ?>
 <div class="modal fade" id="shipping-policy" tabindex="-1" role="dialog" aria-labelledby="shippingPolicyModalLabel" aria-hidden="true">
@@ -82,69 +80,71 @@
       </div>
     </div>
   </div>
-
-    <div class="cart-container" style="min-height: 70vh;">
-      <div class="cart-empty">
-        <div class="empty-img-wrapper text-center">
-          <img src="assets/empty-cart.png" alt="">
-        </div>
-        <div class="btn-continue-wrapper text-center">
-          <a href="index.php"><button type="button" class="btn" style="font-size: 0.9rem;">Continue shopping</button></a>
-        </div>
+<?php if ($cart->getCartArr() == []): ?>
+  <div class="cart-container" style="min-height: 70vh;">
+    <div class="cart-empty">
+      <div class="empty-img-wrapper text-center">
+        <img src="assets/empty-cart.png" alt="">
+      </div>
+      <div class="btn-continue-wrapper text-center">
+        <a href="index.php"><button type="button" class="btn" style="font-size: 0.9rem;">Continue shopping</button></a>
       </div>
     </div>
+  </div>
 
-    <div class="cart-available">
-      <div class="cart-title-wrapper mt-4 pr-4">
-        <div class="row">
-          <div class="col-md-8 d-flex">
-            <h3 class="cart-title ml-3 mt-3 mb-0" style="font-size: 1.4rem;">MY CART</h3>
-            <button type="button" class="btn btn-link remove-all mt-3" data-toggle="modal"
-              data-target="#exampleModal">Remove all</button>
-          </div>
-        </div>
-      </div>
 
+
+  <div class="cart-available">
+    <div class="cart-title-wrapper mt-4 pr-4">
       <div class="row">
-        <ul class="cart-list col-md-8 pr-5">
-
-        </ul>
-        <div class="summary-wrapper col-md-3">
-          <div class="summary card shadow-sm mt-3" style="height: 200px;">
-            <h5 class="text-center mt-2 summary-title ">Order Summary</h5>
-            <div class="tax d-flex mt-4">
-              <h5 class="mr-auto" style="font-weight: 100;">Tax</h5>
-              <p class="mb-0" style="font-size: 0.8rem; font-weight: bold">0₫</p>
-            </div>
-            <hr>
-            <div class="total d-flex">
-              <h5 class="mr-auto" style="font-weight: 100;">Total</h5>
-              <p class="mb-0 total-price"></p>
-            </div>
-          </div>
-          <a href="payment.php"><button type="button" class="btn btn-danger w-100 mt-3 check-out-btn">Checkout</button></a>
-        </div>
-      </div>
-
-      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Remove all products from your cart?</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" style="font-size: 0.8rem;"
-                data-dismiss="modal">Cancel</button>
-              <button type="button" class="btn btn-danger remove-all-btn" style="font-size: 0.8rem;"
-                data-dismiss="modal">Confirm</button>
-            </div>
-          </div>
+        <div class="col-md-8 d-flex">
+          <h3 class="cart-title ml-3 mt-3 mb-0" style="font-size: 1.4rem;">MY CART</h3>
+          <button type="button" class="btn btn-link remove-all mt-3" data-toggle="modal"
+            data-target="#exampleModal">Remove all</button>
         </div>
       </div>
     </div>
 
+    <div class="row">
+      <ul class="cart-list col-md-8 pr-5">
+
+      </ul>
+      <div class="summary-wrapper col-md-3">
+        <div class="summary card shadow-sm mt-3" style="height: 200px;">
+          <h5 class="text-center mt-2 summary-title ">Order Summary</h5>
+          <div class="tax d-flex mt-4">
+            <h5 class="mr-auto" style="font-weight: 100;">Tax</h5>
+            <p class="mb-0" style="font-size: 0.8rem; font-weight: bold">0₫</p>
+          </div>
+          <hr>
+          <div class="total d-flex">
+            <h5 class="mr-auto" style="font-weight: 100;">Total</h5>
+            <p class="mb-0 total-price"></p>
+          </div>
+        </div>
+        <a href="payment.php"><button type="button" class="btn btn-danger w-100 mt-3 check-out-btn">Checkout</button></a>
+      </div>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Remove all products from your cart?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" style="font-size: 0.8rem;"
+              data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-danger remove-all-btn" style="font-size: 0.8rem;"
+              data-dismiss="modal">Confirm</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
 <?php include 'includes/footer.php'; ?>
--- classes/Cart.php
+++ classes/Cart.php
@@ -1,5 +1,4 @@
 <?php
-  include 'includes/config.php';
   class Cart{
     private $conn;
     private $userID;
@@ -16,7 +15,7 @@
     private function addItemToCart($itemID, $price){
       if (checkCart('count') >= 10) $itemOrder = "Item";
       else $itemOrder = "Item0";
-      
+
       $cartArr[$itemOrder] = ["ID"=>$itemID, "price"=>$value, "quantity"=>1];
       $stmt = $conn->prepare("INSERT into cartdetails values (?, ?, ?)");
       $stmt->bind_param("iii",$this->cartID, $itemID, 1);
@@ -24,6 +23,80 @@
       $result = $stmt->get_result();
       if ($result->affected_rows == 1) return true;
       else return false;
+    }
+
+    private function getCartList(){
+      $stmt = $this->conn
+        ("SELECT pri.img1, p.name, p.sold, p.productID, cd.quantity, p.price
+        from cartdetails cd, carts c, products p, productimage pri
+        where cd.cartID = ? and cd.cartID = c.cartID and c.userID = ? and cd.productID = p.productID and p.productID = pri.productID");
+      $stmt->bind_param("ii", $this->cartID, $this->userID);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function printCartList(){
+      $output = ``;
+      $cartList = getCartList();
+      foreach ($cartList as $item) {
+        $output .= `
+        <li class="product-wrapper container card shadow p-2 m-3 d-flex align-items-center justify-content-center">
+          <div class="product d-flex h-100">
+
+            <div class="product-img-wrapper">
+              <img class="product-img" src="{$item['img1']}" alt="product-img">
+            </div>
+
+            <div class="product-info ml-2 d-flex align-items-center">
+              <div class="product-info-wrapper">
+
+                <div class="product-name-wrapper">
+                  <p class="product-name">{$item['name']}</p>
+                </div>
+
+                <div class="product-rating-wrapper">
+
+                  <div class="product-rating">
+                    <span class="fa fa-star text-warning"></span>
+                    <span class="fa fa-star text-warning"></span>
+                    <span class="fa fa-star text-warning"></span>
+                    <span class="fa fa-star text-warning"></span>
+                    <span class="fa fa-star"></span>
+                    <span>({$item['sold']})</span>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="quantity-price-wrapper d-flex align-items-center">
+              <div class="quantity-price w-100">
+                <div class="quantity-control rounded">
+                  <button class="quantity-btn quantity-btn-minus" id="{$item['productID']}" data-toggle="tooltip" data-placement="right" title="Decrease Quantity">
+                    <i class="bi bi-dash"></i>
+                  </button>
+                  <input type="number" class="quantity-input" id="{$item['productID']}" value="{$item['quantity']}" step="1" min="1"  name="quantity">
+                  <button class="quantity-btn quantity-btn-plus" id="{$item['productID']}" data-toggle="tooltip" data-placement="right" title="Increase Quantity">
+                    <i class="bi bi-plus"></i>
+                  </button>
+                </div>
+
+                <div class="product-price-wrapper d-flex align-items-center">
+                  <p href="#" class="product-price m-0">{$item['price']}₫</p>
+                </div>
+              </div>
+            </div>
+
+            <button type="button" class="btn btn-light remove-btn" id="{$item['productID']}" data-toggle="tooltip" data-placement="right" title="Remove Item">
+              <i class="bi bi-x fa-lg"></i>
+            </button>
+
+          </div>
+        </li>
+        `;
+      }
+      return $output;
     }
 
     private function checkCart($case){
@@ -36,7 +109,7 @@
     }
 
     public function getCartArr(){
-      return $cartArr;
+      return $this->cartArr;
     }
   }
 ?>
