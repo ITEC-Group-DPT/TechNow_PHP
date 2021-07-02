2e2471cb0fdea2725b25eb155df764400bfef6c5
chanhbeck <chanhbeck27@gmail.com>
2021-07-02 22:33:03 +0700

    fix minor bug

--- classes/Cart.php
+++ classes/Cart.php
@@ -10,10 +10,10 @@
     }
 
     public function addItemToCart($itemID, $price){
-      if (checkCart('count') >= 10) $itemOrder = "Item";
+      if ($this->checkCart('quantity') >= 10) $itemOrder = "Item";
       else $itemOrder = "Item0";
 
-      $cartArr[$itemOrder] = ["ID"=>$itemID, "price"=>$value, "quantity"=>1];
+      $this->cartArr[$itemOrder] = ["ID"=>$itemID, "price"=>$value, "quantity"=>1];
       $stmt = $conn->prepare("INSERT into cartdetails values (?, ?, ?)");
       $stmt->bind_param("iii",$this->cartID, $itemID, 1);
       $stmt->execute();
@@ -98,9 +98,9 @@
 
     public function checkCart($case){
       $total = 0;
-      foreach ($cartArr as $item) {
+      foreach ($this->cartArr as $item) {
         if ($case == 'price') $total .= $item['price'];
-        elseif ($case == 'count') $total .= $item['quantity'];
+        elseif ($case == 'quantity') $total .= $item['quantity'];
       }
       return $total;
     }
