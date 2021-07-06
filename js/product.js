783dd7b60ef4f82a3cc33216b5451491ec5e54d8
chanhbeck <chanhbeck27@gmail.com>
2021-07-06 23:49:26 +0700

    nevermind

--- js/product.js
+++ js/product.js
@@ -9,7 +9,6 @@ $(document).ready(() => {
 
 function favoriteFunc(){
     let value = favorite.getAttribute("data-value");
-    if (value == '') return false;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "ajaxFavorite.php", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
