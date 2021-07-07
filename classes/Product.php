c37d2c338af55f24108bd08d2c528aaeaaa7c690
JustTory <johnnynathany@gmail.com>
2021-07-07 10:45:53 +0700

    test

--- classes/Product.php
+++ classes/Product.php
@@ -12,14 +12,25 @@
 
 
         //constructor
-        public function __construct($conn)
-        {
+        public function __construct($conn) {
             $this->conn = $conn;
+        }
+
+        public function getProductByID($id) {
+            $sql = "SELECT FROM products WHERE productID = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows == 1) {
+                return $result->fetch_assoc();
+            }
+            else return false;
         }
 
         //static
         public static function getProductsByCategory($type,&$conn,$limit = 20, $offset = 0) {
-            $sql = "SELECT * FROM products p, productimage pimg 
+            $sql = "SELECT * FROM products p, productimage pimg
             WHERE p.type = ? and p.productID = pimg.productID LIMIT ?,?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("sii", $type,$offset,$limit);
@@ -33,7 +44,7 @@
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("i",$limit);
             $stmt->execute();
-            
+
             $results = $stmt->get_result();
             return $results->fetch_all(MYSQLI_ASSOC);
         }
---
+++ product.php
@@ -0,0 +1,20 @@
+<?php
+    include "./includes/config.php";
+    include "./includes/header.php";
+    include "./classes/Product.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+
+        </div>
+
+        <div class="col-md-6">
+
+        </div>
+    </div>
+</div>
+<?php
+    include "includes/footer.php";
+?>
