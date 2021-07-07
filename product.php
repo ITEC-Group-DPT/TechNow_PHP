7c235b8daae75fd1ede5003cee2489df359d8c34
JustTory <johnnynathany@gmail.com>
2021-07-07 14:15:35 +0700

    UI hmu hmu :(

--- product.php
+++ product.php
@@ -10,39 +10,47 @@
         $ratingStar = getStarRating(intval($res['rating']));
         $format_price = number_format($res['price'],0);
     }
-
-    //var_dump($res);
 ?>
 
-<div class="main-container mt-5" style="min-height: 80vh;">
+<div class="main-container mt-5 rounded shadow" style="min-height: 80vh;">
     <div class="row">
     <?php if ($res != false): ?>
         <div class="col-md-6">
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
+                <?php if (!empty($res['img2'])): ?>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="bg-secondary active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="bg-secondary"></li>
+                        <?php if (!empty($res['img3'])): ?>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2" class="bg-secondary"></li>
+                        <?php endif; ?>
+                    </ol>
+                <?php endif; ?>
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="<?php echo $res['img1'] ?>" alt="First slide">
+                    <div class="carousel-item w-100 active">
+                        <img class="image d-block w-100 h-100" src="<?php echo $res['img1'] ?>" alt="First slide">
                     </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo $res['img2'] ?>" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo $res['img3'] ?>" alt="Third slide">
-                    </div>
+                    <?php if (!empty($res['img2'])): ?>
+                        <div class="carousel-item w-100">
+                            <img class="image d-block w-100 h-100" src="<?php echo $res['img2'] ?>" alt="Second slide">
+                        </div>
+                    <?php endif; ?>
+                    <?php if (!empty($res['img3'])): ?>
+                        <div class="carousel-item w-100">
+                            <img class="image d-block w-100 h-100" src="<?php echo $res['img3'] ?>" alt="Third slide">
+                        </div>
+                    <?php endif; ?>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <?php if (!empty($res['img2'])): ?>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="" aria-hidden="true"><i class="bi bi-arrow-left-short text-black-50 fa-2x"></i></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="" aria-hidden="true"><i class="bi bi-arrow-right-short text-black-50 fa-2x"></i></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                <?php endif; ?>
             </div>
         </div>
 
@@ -56,8 +64,8 @@
                 <p class="product-desc"><?php echo $res['description'] ?></p>
                 <h5 class="product-title mt-4">Specs</h5>
                 <p class="product-spec"><?php echo $res['spec'] ?></p>
-                <p class='price mt-5 text-right'><?php echo $format_price; ?> đ</p>
-
+                <hr class="mt-5">
+                <p class='price text-right'><?php echo $format_price; ?> đ</p>
                 <div class="functions d-flex justify-content-end mt-3">
                     <button type="button" class="btn btn-primary add-favorite mr-3" id="<?php echo $res['productID']; ?>"><i class="bi bi-heart"></i> Add to Favorite</button>
                     <button type="button" class="btn btn-primary add-cart" id="<?php echo $res['productID']; ?>"><i class="bi bi-cart-plus"></i> Add to Cart</button>
