2fe2f8728e6439be6ae9f305614d497f18d8cc3c
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-09-28 22:06:01 +0700

    Revert "Update header_func.php"
    This reverts commit a4b63ada98bfc2d0b00222af9ed6c31c40374957.

--- functions/header_func.php
+++ functions/header_func.php
@@ -1,7 +1,7 @@
 <?php
     function chooseCSSFile($page_name){
         $css = "css/";
-        if ($page_name == "index" || $page_name =='Index')
+        if ($page_name == "index")
             $css .= "style";
         else $css .= $page_name;
         $css .= ".css";
